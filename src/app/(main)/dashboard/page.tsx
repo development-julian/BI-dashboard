cd781c39fb9513f9a380082bb1986ce2b1f1dae1
Julian Velasquez <development@growtzy.com>
2026-01-12 16:27:53 +0000

    necesito ue las metricas de 'ad spent', 'ROAS'  y  'CPL' ocupen el mismo

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -27,48 +27,41 @@ export default function DashboardPage() {
 
   if (loading || !data) {
     return (
-        <div className="flex flex-col gap-6">
-            <Skeleton className="h-[140px] w-full" />
-            <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+      <div className="flex flex-col gap-6">
+        <Skeleton className="h-[140px] w-full" />
+        <div className="grid grid-cols-1 gap-6 lg:grid-cols-12">
+          <div className="lg:col-span-8 flex flex-col gap-6">
+            <div className="grid gap-6 md:grid-cols-3">
                 <Skeleton className="h-[120px] w-full" />
                 <Skeleton className="h-[120px] w-full" />
                 <Skeleton className="h-[120px] w-full" />
             </div>
-            <div className="grid grid-cols-1 gap-6 lg:grid-cols-12">
-                <Skeleton className="h-[400px] w-full lg:col-span-8" />
-                <Skeleton className="h-[400px] w-full lg:col-span-4" />
-            </div>
-             <div className="grid grid-cols-1 gap-6 lg:grid-cols-12">
-                <Skeleton className="h-[300px] w-full lg:col-span-8" />
-                <Skeleton className="h-[300px] w-full lg:col-span-4" />
-            </div>
+            <Skeleton className="h-[400px] w-full" />
+            <Skeleton className="h-[300px] w-full" />
+          </div>
+          <div className="lg:col-span-4 flex flex-col gap-6">
+             <Skeleton className="h-[400px] w-full" />
+             <Skeleton className="h-[300px] w-full" />
+          </div>
         </div>
+      </div>
     );
   }
-
 
   return (
     <div className="flex flex-col gap-6">
       <AiForecast />
 
-      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-        <KpiCards kpis={data.kpis} />
-      </div>
-
       <div className="grid grid-cols-1 gap-6 lg:grid-cols-12">
-        <div className="lg:col-span-8">
+        <div className="lg:col-span-8 flex flex-col gap-6">
+          <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+            <KpiCards kpis={data.kpis} />
+          </div>
           <LeadConversionTrends data={data.leadConversion} />
-        </div>
-        <div className="lg:col-span-4">
-          <FunnelPerformance data={data.funnelPerformance} />
-        </div>
-      </div>
-
-      <div className="grid grid-cols-1 gap-6 lg:grid-cols-12">
-        <div className="lg:col-span-8">
           <SalesByChannel data={data.salesByChannel} />
         </div>
-        <div className="lg:col-span-4">
+        <div className="lg:col-span-4 flex flex-col gap-6">
+          <FunnelPerformance data={data.funnelPerformance} />
           <ProductPerformance data={data.productPerformance} />
         </div>
       </div>
